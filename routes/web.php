2bf753a132b3efe8d01d02e0edd4b0a49e847802
fallenprodigy <giorgi.kalatozishvili.1@btu.edu.ge>
2023-02-22 23:53:29 +0400

    routing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/hello", function() {
+    return "Hello World";
+});
+
+Route::get("/posts/{id}", function($id) {
+    return response("Post " . $id);
+})->where("id", "[0-9]+");
