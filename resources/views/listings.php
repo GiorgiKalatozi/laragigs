f0184422feb7228fa956d4dddcc5e6c97646f09a
fallenprodigy <giorgi.kalatozishvili.1@btu.edu.ge>
2023-02-25 22:15:31 +0400

    blade templates basic directives

--- resources/views/listings.php
+++
@@ -1,5 +0,0 @@
-<h1><?php echo $heading ?></h1>
-<?php foreach($listings as $listing): ?>
- <h3><?php echo $listing["title"] ?></h3>
- <p><?php echo $listing["description"] ?></p>
-     <?php endforeach; ?>
