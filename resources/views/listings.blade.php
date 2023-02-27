86efc355f72d8c08a50c1f2fc19e1410d9607d59
fallenprodigy <giorgi.kalatozishvili.1@btu.edu.ge>
2023-02-28 03:43:54 +0400

    layout and sections

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,10 +1,14 @@
+@extends("layout")
+
+@section("content")
 <h1>{{ $heading }}</h1>
 
 @unless (count($listings) == 0)
 @foreach ($listings as $listing )
-<h2>{{ $listing["title"] }}</h2>
+<h2><a href="/{{ $listing["id"] }}">{{ $listing["title"] }}</a></h2>
 <p>{{ $listing["description"] }}</p>
 @endforeach
 @else
 <p>No Listings Found</p>
 @endunless
+@endsection
