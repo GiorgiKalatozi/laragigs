8b38bb9bbcec03d0789a82b3b7379fcda4129d80
fallenprodigy <giorgi.kalatozishvili.1@btu.edu.ge>
2023-03-03 19:47:30 +0400

    auth links

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -33,9 +33,23 @@
                 ><img class="w-24" src="{{ asset("images/logo.png") }}" alt="" class="logo"
             /></a>
             <ul class="flex space-x-6 mr-6 text-lg">
+                @auth
+                    
+                <li>
+                    <span class="font-bold uppercase">
+                        Welcome {{ auth()->user()->name }}
+                    </span>
+                </li>
+                <li>
+                    <a href="/listings/manage" class="hover:text-laravel"
+                    ><i class="fa-solid fa-gear"></i>
+                    Manage Listings</a
+                    >
+                </li>
+                @else
                 <li>
                     <a href="/register" class="hover:text-laravel"
-                        ><i class="fa-solid fa-user-plus"></i> Register</a
+                    ><i class="fa-solid fa-user-plus"></i> Register</a
                     >
                 </li>
                 <li>
@@ -44,18 +58,19 @@
                         Login</a
                     >
                 </li>
+                @endauth
             </ul>
         </nav>
      <main>
           {{ $slot }}
      </main>
      <footer
-               class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center"
+     class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center"
           >
                <p class="ml-2">Copyright &copy; 2023, All Rights reserved</p>
 
                <a
-                    href="listings/create"
+               href="listings/create"
                     class="absolute top-1/3 right-10 bg-black text-white py-2 px-5"
                     >Post Job</a
                >
