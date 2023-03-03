01f0f6d3cc3928a386182f6be6a3beb9c568c5bf
fallenprodigy <giorgi.kalatozishvili.1@btu.edu.ge>
2023-03-04 02:29:01 +0400

    user authorization

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -64,7 +64,9 @@ class ListingController extends Controller
     public function update(Request $request, Listing $listing) {
 
         // Make sure logged in user is owner
-        
+        if ($listing->user_id != auth()->id()) {
+            abort(403, "Unauthorized Action");
+        }
 
         $formFields = $request->validate([
             "title" => "required",
@@ -87,6 +89,12 @@ class ListingController extends Controller
 
     // Delete Listing
     public function destroy(Listing $listing) {
+        
+         // Make sure logged in user is owner
+        if ($listing->user_id != auth()->id()) {
+            abort(403, "Unauthorized Action");
+        } 
+
         $listing->delete();
         return redirect("/")->with("message", "Listing deleted successfully ");
     }
